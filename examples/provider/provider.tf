1185ca8f7ee242cea464795767b1228d8aa100c5
Jonathan Ballet <jon@multani.info>
2023-01-03 18:32:10 +0100

    Improve documentation

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,8 @@
-provider "scaffolding" {
-  # example configuration here
+# Get the client ID and client secret from https://console.cloud.camunda.io/
+variable "camunda_client_id" {}
+variable "camunda_client_secret" {}
+
+provider "camunda" {
+  client_id     = var.camunda_client_id
+  client_secret = var.camunda_client_secret
 }
